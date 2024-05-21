1ba4945434360becdb7c9369ca1db0fb07f48b61
Samarth <samarthgalchar@gmail.com>
2024-05-21 14:57:43 -0400

    added github to toc

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,14 +1,14 @@
 function TOC() {
-    return (
-      <ul>
-        <li><a id="wd-a"  href="#/Labs">Labs</a></li>
-        <li><a id="wd-a1" href="#/Labs/Lab1">Lab 1</a></li>
-        <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
-        <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
-        <li><a id="wd-a4" href="#/Kanbas/">Kanbas</a></li>
-      </ul>
-    );
-  }
+  return (
+    <ul>
+      <li><a id="wd-a" href="#/Labs">Labs</a></li>
+      <li><a id="wd-a1" href="#/Labs/Lab1">Lab 1</a></li>
+      <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
+      <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
+      <li><a id="wd-a4" href="#/Kanbas/">Kanbas</a></li>
+      <li><a href="https://github.com/Samarthvg/kanbas-react-web-app">Github</a></li>
+    </ul>
+  );
+}
 
-  export default TOC;
-  
+export default TOC;
