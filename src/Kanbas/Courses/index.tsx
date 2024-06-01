571e508d4296d48a6cb61c8f1910a0b19d8fbed9
Samarth <samarthgalchar@gmail.com>
2024-06-02 03:45:38 +0530

    completed navigation and started dashboard

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -7,27 +7,26 @@ import AssignmentEditor from "./Assignments/Editor";
 export default function Courses() {
   return (
     <div id="wd-courses">
-      <h2>Course 1234</h2>
-      <hr />
-      <table>
-        <tr>
-          <td valign="top">
-            <CoursesNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/"
-                     element={<Navigate to="Home" />} />
-              <Route path="Home" element={<Home />} />
-              <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments"
-                     element={<Assignments />} />
-              <Route path="Assignments/:id"
-                     element={<AssignmentEditor />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+  <h2 className="text-danger me-4 fs-4 mb-1 fa-align-justify"> 
+      Course 1234</h2>
+  <hr />
+  
+  <div className="d-flex">
+    <div className="d-none d-md-block">
+      <CoursesNavigation />
     </div>
+    <div className="flex-fill">
+   
+      <Routes>
+        <Route path="Home" element={<Home />} />
+        <Route path="Modules" element={<Modules />} />
+        <Route path="Assignments" element={<Assignments />} />
+        <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+      </Routes>
+    </div>
+  </div>
+  
+</div>
+
   );
 }
