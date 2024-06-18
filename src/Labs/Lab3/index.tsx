7e8ef2dd3600efd3441312816311036107adfa5d
Samarth <samarthgalchar@gmail.com>
2024-06-18 11:53:36 -0400

    Labs upto functions

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,8 +1,28 @@
-function Lab3(){
-    return(
-        <div>
-            <h2>Lab 3</h2>
-        </div>
-    )
+import BooleanVariables from "./BooleanVariables";
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputInline from "./ConditionalOutputIfElse";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+
+export default function Lab3() {
+  return(
+    <div id="wd-lab3" className="container-fluid">
+      <h3>Lab 3</h3>
+      <hr />
+      <VariablesAndConstants/>
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+    </div>
+  );
 }
-export default Lab3;
+
