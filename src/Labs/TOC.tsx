bd48cc78e4df33de97425562053ebbebb18fe480
Samarth <samarthgalchar@gmail.com>
2024-06-19 04:39:37 -0400

    completed labs

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router";
+
 export default function TOC() {
   const { pathname } = useLocation();
   return (
@@ -10,10 +11,7 @@ export default function TOC() {
       </li>
       <li className="nav-item">
         <a id="wd-a1" href="#/Labs/Lab1"
-          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
-          Lab 1
-        </a>
-      </li>
+        className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>Lab 1</a></li>
       <li className="nav-item">
         <a id="wd-a2" href="#/Labs/Lab2"
           className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
